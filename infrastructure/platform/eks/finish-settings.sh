9785175fa0957fa64772a75d04344300a179d00e
Juan Mesa <linuxven@gmail.com>
2020-07-03 08:26:38 +0200

    change helm command

--- infrastructure/platform/eks/finish-settings.sh
+++ infrastructure/platform/eks/finish-settings.sh
@@ -4,7 +4,7 @@ echo "Setting up ALB Ingress Controller RBAC"
 kubectl apply -f alb-ingress-controller-rbac.yaml
 
 echo "Installing ALB Ingress Controller via Helm"
-helm3 upgrade ingress-controller incubator/aws-alb-ingress-controller --debug --install --wait --version 1.0.0 --namespace kube-system --values alb-ingress-controller.yaml
+helm upgrade ingress-controller incubator/aws-alb-ingress-controller --debug --install --wait --version 1.0.0 --namespace kube-system --values alb-ingress-controller.yaml
 
 echo "Setting up namespaces"
 for ns in monitoring jenkins qa prod; do kubectl create ns $ns; done 
